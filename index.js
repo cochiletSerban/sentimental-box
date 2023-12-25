c62a6fb1a0117cc37b2ac23f3f01d3cbf282ae2a
cserban <cserban@lenovo.com>
2023-12-25 20:50:33 +0200

    e2e test

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
+require("dotenv").config();
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const mqtt = require("mqtt");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+let lastPing = Date.now();
+let ack = false;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, "public")));
+mongoose.connect(
+  `mongodb+srv://dark:${process.env.dbPass}@cluster0.2hyuk3e.mongodb.net/?retryWrites=true&w=majority`
+);
+const Message = mongoose.model("Message", { content: String, timestamp: Date });
 
-// Define a route for the root URL
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const mqttClient = mqtt.connect(process.env.mqttBroker);
+const mqttPublishTopic = process.env.mqttPublishTopic;
+const mqttSubscribeTopic = process.env.mqttSubscribeTopic;
+
+mqttClient.on("connect", () => {
+  mqttClient.subscribe(mqttSubscribeTopic);
+});
+mqttClient.on("message", (topic, message) => {
+  if (message.toString().startsWith("H")) lastPing = Date.now();
+  else ack = true;
+  console.log(lastPing, ack);
 });
 
-// Start the server
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/messages", async (req, res) => {
+  try {
+    const messages = await Message.find().sort({ timestamp: "desc" });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/messages/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedMessage = await Message.findByIdAndDelete(id);
+
+    if (!deletedMessage) {
+      return res.status(404).json({ error: "Message not found" });
+    }
+
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/publish", async (req, res) => {
+  ack = false;
+  let message = { message: "Message saved and published successfully" };
+  try {
+    const { content } = req.body;
+
+    const newMessage = new Message({ content, timestamp: new Date() });
+    await newMessage.save();
+
+    await mqttClient.publishAsync(mqttPublishTopic, content);
+
+    if (!ack) {
+      message = { message: "Message NOT saved and published successfully" };
+    }
+
+    res.status(200).json(message);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
